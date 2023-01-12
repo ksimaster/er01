d496a4e75887750ec3b91455dc25545e298983ac
IIEC-kycaka <34373675+IIEC-kycaka@users.noreply.github.com>
2023-01-12 11:09:48 +0300

    finish

---
+++ Assets/Scr/History_Papper1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class History_Papper1 : MonoBehaviour
+{
+    public GameObject history_paper1;
+    public GameObject player;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "History1")
+                {
+                    FindObjectOfType<AudioManager>().Play("Paper");
+                    history_paper1.gameObject.SetActive(!history_paper1.gameObject.activeSelf);
+                    player.GetComponent<Move>().enabled = !player.GetComponent<Move>().enabled;
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scr/History_Papper2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class History_Papper2 : MonoBehaviour
+{
+    public GameObject history_paper2;
+    public GameObject player;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "History2")
+                {
+                    FindObjectOfType<AudioManager>().Play("Paper");
+                    history_paper2.gameObject.SetActive(!history_paper2.gameObject.activeSelf);
+                    player.GetComponent<Move>().enabled = !player.GetComponent<Move>().enabled;
+
+                }
+            }
+        }
+    }
+}
